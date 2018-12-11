96e5baa842f36f2e3d1b43da98c993d6299f1dab
WalternativE <gregor.beyerle@gmail.com>
2018-12-11 23:05:27 +0100

    raw implementation of core application

--- src/App.fs
+++ src/App.fs
@@ -7,39 +7,89 @@ open Fable.Helpers.React.Props
 
 // MODEL
 
-type Model = int
+type Draft =
+    | NewDraft of string
+    | BumpedDraft of string * int
+    | RejectedDRaft of string
+
+type Model =
+    { DraftForm : string
+      Drafts : string list }
 
 type Msg =
-| Increment
-| Decrement
+| UpdateDraftForm of string
+| CreateDraft
 
-let init() : Model = 0
+let init() : Model =
+    { DraftForm = ""
+      Drafts = [] }
 
 // UPDATE
 
 let update (msg:Msg) (model:Model) =
     match msg with
-    | Increment -> model + 1
-    | Decrement -> model - 1
+    | UpdateDraftForm content ->
+        { model with DraftForm = content }
+    | CreateDraft ->
+        { model with
+            DraftForm = ""
+            Drafts = model.DraftForm::model.Drafts }
 
 // VIEW (rendered with React)
 
 open Fulma
 
+let toCards (title : string) =
+    Tile.tile [ Tile.IsChild; Tile.Size Tile.Is4; Tile.CustomClass "content-card" ]
+        [ Card.card [ ]
+            [ Card.header []
+                [ Card.Header.title [] [ str title ] ]
+              Card.content []
+                [ Content.content [] [ str "A generic conent" ] ]
+              Card.footer []
+                [ Card.Footer.a [] [ str "Do It" ] ] ] ]
+
+let toCardRow row =
+    Tile.tile [ Tile.IsParent; Tile.Size Tile.Is12 ] row
+
+let rec chunkByThree soFar l =
+    match l with
+    | x1::x2::[x3] ->
+        [x1; x2; x3]::soFar
+    | x1::x2::x3::xs ->
+        chunkByThree ([x1; x2; x3]::soFar) xs
+    | xs ->
+        xs::soFar
+
+let toCardRows (titles : string list) =
+    titles
+    |> chunkByThree []
+    |> List.rev
+    |> List.map ((List.map toCards) >> toCardRow)
+
 let view (model:Model) dispatch =
 
     div []
-      [ Hero.hero [ Hero.Color IsSuccess; Hero.IsHalfHeight ]
-          [ Hero.body []
-              [ Container.container [ Container.IsFullHD
-                                      Container.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                  [ Heading.h1 []
-                      [ str "Glory to the Counter!" ] ] ] ]
+      [ Navbar.navbar [ Navbar.Color IsBlack ]
+            [ Navbar.Brand.div []
+                [ Navbar.Item.a [ Navbar.Item.Props [ Href "#" ] ]
+                    [ str "Card Manager" ] ] ]
         Container.container [ Container.IsFluid ]
-          [ Content.content []
-              [ button [ OnClick (fun _ -> dispatch Increment) ] [ str "PLUS" ]
-                div [] [ str (string model) ]
-                button [ OnClick (fun _ -> dispatch Decrement) ] [ str "MINUS" ] ] ] ]
+          [ h1 [ Class "is-size-1" ] [ str "Manage your Cards" ]
+            Tile.tile [ Tile.IsAncestor; Tile.IsVertical ]
+                [ Tile.tile [ Tile.IsParent; Tile.Size Tile.Is12 ]
+                    [ Tile.tile [ Tile.IsChild ]
+                        [ Card.card []
+                            [ Card.header []
+                                [ Card.Header.title [] [ str "Write a draft!" ] ]
+                              Card.content []
+                                [ Input.text [ Input.Placeholder "Your draft"
+                                               Input.Value model.DraftForm
+                                               Input.OnChange (fun ev -> UpdateDraftForm ev.Value |> dispatch) ] ]
+                              Card.footer []
+                                [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> dispatch CreateDraft) ] ]
+                                    [ str "Submit" ] ] ] ] ]
+                  model.Drafts |> toCardRows |> ofList ] ] ]
 
 #if DEBUG
 open Elmish.Debug
