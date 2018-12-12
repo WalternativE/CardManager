e6b4fe98897071d407e78169b453ccbef7d8ed86
WalternativE <gregor.beyerle@gmail.com>
2018-12-12 14:13:46 +0100

    implements rest of base functionality

--- src/App.fs
+++ src/App.fs
@@ -20,12 +20,41 @@ type Msg =
 | UpdateDraftForm of string
 | CreateDraft
 | BumpDraft of string
+| UnbumpDraft of string
+| RejectDraft of string
+| DeleteDraft of string
 
 let init() : Model =
     { DraftForm = ""
       Drafts = [] }
 
 // UPDATE
+
+let bump (title : string) (d : Draft) =
+    match d with
+    | NewDraft t ->
+        if t = title then (BumpedDraft (t, 1)) else d
+    | BumpedDraft (t, b) ->
+        if t = title then (BumpedDraft (t, b + 1)) else d
+    | RejectedDraft _ -> d
+
+let unbump (title : string) (d : Draft) =
+    match d with
+    | NewDraft _ -> d
+    | BumpedDraft (t, b) when b > 1 ->
+        if t = title then
+            (BumpedDraft (t, b - 1))
+        else d
+    | BumpedDraft (t, _) ->
+        NewDraft t
+    | RejectedDraft _ -> d
+
+let reject (title : string) (d : Draft) =
+    match d with
+    | NewDraft t ->
+        if t = title then (RejectedDraft t) else d
+    | BumpedDraft _ -> d
+    | RejectedDraft _ -> d
 
 let update (msg:Msg) (model:Model) =
     match msg with
@@ -39,13 +68,26 @@ let update (msg:Msg) (model:Model) =
     | BumpDraft title ->
         let drafts =
             model.Drafts
-            |> List.map (fun d ->
-                  match d with
-                  | NewDraft t ->
-                        if t = title then (BumpedDraft (t, 1)) else d
-                  | BumpedDraft (t, b) ->
-                        if t = title then (BumpedDraft (t, b + 1)) else d
-                  | RejectedDraft _ -> d)
+            |> List.map (bump title)
+        { model with Drafts = drafts }
+    | UnbumpDraft title ->
+        let drafts =
+            model.Drafts
+            |> List.map (unbump title)
+        { model with Drafts = drafts }
+    | RejectDraft title ->
+        let drafts = 
+            model.Drafts
+            |> List.map (reject title)
+        { model with Drafts = drafts }
+    | DeleteDraft title ->
+        let drafts =
+            model.Drafts
+            |> List.filter
+                (fun d ->
+                    match d with
+                    | RejectedDraft t -> t = title |> not
+                    | _ -> true)
         { model with Drafts = drafts }
 
 // VIEW (rendered with React)
@@ -62,9 +104,9 @@ let toCard dispatch (draft : Draft) =
                   Card.content []
                     [ Content.content [] [ str "Your prestine card draft." ] ]
                   Card.footer []
-                    [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> BumpDraft title |> dispatch ) ] ]
+                    [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> BumpDraft title |> dispatch) ] ]
                         [ str "Bump" ]
-                      Card.Footer.a []
+                      Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> RejectDraft title |> dispatch) ] ]
                         [ str "Reject" ] ] ] ]
     | BumpedDraft (title, bumps) ->
         Tile.tile [ Tile.IsChild; Tile.Size Tile.Is4; Tile.CustomClass "content-card" ]
@@ -74,9 +116,10 @@ let toCard dispatch (draft : Draft) =
                   Card.content []
                     [ Content.content [] [ sprintf "This card has %d bumps!" bumps |> str ] ]
                   Card.footer []
-                    [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> BumpDraft title |> dispatch ) ] ]
+                    [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> BumpDraft title |> dispatch) ] ]
                         [ str "Bump" ]
-                      Card.Footer.a [] [ str "Reject" ] ] ] ]
+                      Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> UnbumpDraft title |> dispatch) ] ]
+                        [ str "Unbump" ] ] ] ]
     | RejectedDraft title ->
         Tile.tile [ Tile.IsChild; Tile.Size Tile.Is4; Tile.CustomClass "content-card" ]
             [ Card.card [ ]
@@ -85,10 +128,8 @@ let toCard dispatch (draft : Draft) =
                   Card.content []
                     [ Content.content [] [ str "Unfortunately this draft has been rejected 🙁" ] ]
                   Card.footer []
-                    [ Card.Footer.a []
-                        [ str "Bump" ]
-                      Card.Footer.a []
-                        [ str "Reject" ] ] ] ]
+                    [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> DeleteDraft title |> dispatch) ] ]
+                        [ str "Delete" ] ] ] ]
 
 let toCardRow row =
     Tile.tile [ Tile.IsParent; Tile.Size Tile.Is12 ] row
@@ -116,7 +157,7 @@ let view (model:Model) dispatch =
                 [ Navbar.Item.a [ Navbar.Item.Props [ Href "#" ] ]
                     [ str "Card Manager" ] ] ]
         Container.container [ Container.IsFluid ]
-          [ h1 [ Class "is-size-1" ] [ str "Manage your Cards" ]
+          [ h1 [ Class "is-size-1 app-title" ] [ str "Manage your Cards" ]
             Tile.tile [ Tile.IsAncestor; Tile.IsVertical ]
                 [ yield Tile.tile [ Tile.IsParent; Tile.Size Tile.Is12 ]
                     [ Tile.tile [ Tile.IsChild ]
