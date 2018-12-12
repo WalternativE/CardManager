23a9bf3a80f7bf74d50b168a21a921f59869b096
WalternativE <gregor.beyerle@gmail.com>
2018-12-12 12:28:56 +0100

    implements different card types

--- src/App.fs
+++ src/App.fs
@@ -10,15 +10,16 @@ open Fable.Helpers.React.Props
 type Draft =
     | NewDraft of string
     | BumpedDraft of string * int
-    | RejectedDRaft of string
+    | RejectedDraft of string
 
 type Model =
     { DraftForm : string
-      Drafts : string list }
+      Drafts : Draft list }
 
 type Msg =
 | UpdateDraftForm of string
 | CreateDraft
+| BumpDraft of string
 
 let init() : Model =
     { DraftForm = ""
@@ -31,23 +32,63 @@ let update (msg:Msg) (model:Model) =
     | UpdateDraftForm content ->
         { model with DraftForm = content }
     | CreateDraft ->
+        let newDraft = NewDraft model.DraftForm
         { model with
             DraftForm = ""
-            Drafts = model.DraftForm::model.Drafts }
+            Drafts = newDraft::model.Drafts }
+    | BumpDraft title ->
+        let drafts =
+            model.Drafts
+            |> List.map (fun d ->
+                  match d with
+                  | NewDraft t ->
+                        if t = title then (BumpedDraft (t, 1)) else d
+                  | BumpedDraft (t, b) ->
+                        if t = title then (BumpedDraft (t, b + 1)) else d
+                  | RejectedDraft _ -> d)
+        { model with Drafts = drafts }
 
 // VIEW (rendered with React)
 
 open Fulma
 
-let toCards (title : string) =
-    Tile.tile [ Tile.IsChild; Tile.Size Tile.Is4; Tile.CustomClass "content-card" ]
-        [ Card.card [ ]
-            [ Card.header []
-                [ Card.Header.title [] [ str title ] ]
-              Card.content []
-                [ Content.content [] [ str "A generic conent" ] ]
-              Card.footer []
-                [ Card.Footer.a [] [ str "Do It" ] ] ] ]
+let toCard dispatch (draft : Draft) =
+    match draft with
+    | NewDraft title ->
+        Tile.tile [ Tile.IsChild; Tile.Size Tile.Is4; Tile.CustomClass "content-card" ]
+            [ Card.card [ ]
+                [ Card.header []
+                    [ Card.Header.title [] [ str title ] ]
+                  Card.content []
+                    [ Content.content [] [ str "Your prestine card draft." ] ]
+                  Card.footer []
+                    [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> BumpDraft title |> dispatch ) ] ]
+                        [ str "Bump" ]
+                      Card.Footer.a []
+                        [ str "Reject" ] ] ] ]
+    | BumpedDraft (title, bumps) ->
+        Tile.tile [ Tile.IsChild; Tile.Size Tile.Is4; Tile.CustomClass "content-card" ]
+            [ Card.card [ ]
+                [ Card.header []
+                    [ Card.Header.title [] [ str title ] ]
+                  Card.content []
+                    [ Content.content [] [ sprintf "This card has %d bumps!" bumps |> str ] ]
+                  Card.footer []
+                    [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> BumpDraft title |> dispatch ) ] ]
+                        [ str "Bump" ]
+                      Card.Footer.a [] [ str "Reject" ] ] ] ]
+    | RejectedDraft title ->
+        Tile.tile [ Tile.IsChild; Tile.Size Tile.Is4; Tile.CustomClass "content-card" ]
+            [ Card.card [ ]
+                [ Card.header []
+                    [ Card.Header.title [] [ str title ] ]
+                  Card.content []
+                    [ Content.content [] [ str "Unfortunately this draft has been rejected 🙁" ] ]
+                  Card.footer []
+                    [ Card.Footer.a []
+                        [ str "Bump" ]
+                      Card.Footer.a []
+                        [ str "Reject" ] ] ] ]
 
 let toCardRow row =
     Tile.tile [ Tile.IsParent; Tile.Size Tile.Is12 ] row
@@ -61,11 +102,11 @@ let rec chunkByThree soFar l =
     | xs ->
         xs::soFar
 
-let toCardRows (titles : string list) =
+let toCardRows dispatch (titles : Draft list) =
     titles
     |> chunkByThree []
     |> List.rev
-    |> List.map ((List.map toCards) >> toCardRow)
+    |> List.map ((List.map (toCard dispatch)) >> toCardRow)
 
 let view (model:Model) dispatch =
 
@@ -77,7 +118,7 @@ let view (model:Model) dispatch =
         Container.container [ Container.IsFluid ]
           [ h1 [ Class "is-size-1" ] [ str "Manage your Cards" ]
             Tile.tile [ Tile.IsAncestor; Tile.IsVertical ]
-                [ Tile.tile [ Tile.IsParent; Tile.Size Tile.Is12 ]
+                [ yield Tile.tile [ Tile.IsParent; Tile.Size Tile.Is12 ]
                     [ Tile.tile [ Tile.IsChild ]
                         [ Card.card []
                             [ Card.header []
@@ -85,11 +126,14 @@ let view (model:Model) dispatch =
                               Card.content []
                                 [ Input.text [ Input.Placeholder "Your draft"
                                                Input.Value model.DraftForm
-                                               Input.OnChange (fun ev -> UpdateDraftForm ev.Value |> dispatch) ] ]
+                                               Input.OnChange (fun ev -> UpdateDraftForm ev.Value |> dispatch)
+                                               Input.Option.Props
+                                                 [ OnKeyUp (fun ev ->
+                                                        if ev.keyCode = 13. then dispatch CreateDraft) ] ] ]
                               Card.footer []
                                 [ Card.Footer.a [ GenericOption.Props [ OnClick (fun _ -> dispatch CreateDraft) ] ]
                                     [ str "Submit" ] ] ] ] ]
-                  model.Drafts |> toCardRows |> ofList ] ] ]
+                  yield! model.Drafts |> toCardRows dispatch ] ] ]
 
 #if DEBUG
 open Elmish.Debug
